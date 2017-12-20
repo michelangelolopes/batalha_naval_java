77701d7bc7fb641c555199c08e256080cb156d99
michelangelolopes <34664670+michelangelolopes@users.noreply.github.com>
2017-12-20 13:43:06 -0200

    Update Arquivo.java

--- BatalhaNaval/src/classes/Arquivo.java
+++ BatalhaNaval/src/classes/Arquivo.java
@@ -43,6 +43,7 @@ public final class Arquivo
         procurarArquivo = new JFileChooser();        
         arquivoSelecionado = abrirDiretorio(procurarArquivo);
         File arquivo;
+        texto = " ";
         
         if (arquivoSelecionado == JFileChooser.APPROVE_OPTION) 
         {
